5cd09c4f0cf884e43a2a830a78d49e51ddd118cd
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-07-27 16:23:04 +0800

    005 resolved TLE

--- 005_insertion-sort-list.py
+++ 005_insertion-sort-list.py
@@ -10,28 +10,34 @@ class Solution:
     # @return a ListNode
 
     # ascending
-    def insertionSortList(self, head):
+    def insertionSortList_TLE(self, head):
+        """
+        Known issue: Time Limit Excedded
+        """
         comparator = lambda x, y: cmp(x.val, y.val)
-        # open set and closed set
+        # open set & closed set
         # iterate through all the nodes
-        dummy_head = ListNode(-1<<32)
+        dummy_head = ListNode(0)
         dummy_head.next = head
 
 
-        closed_tail = head
+        closed_tail = dummy_head.next
         while(closed_tail and closed_tail.next):
             open_head = closed_tail.next
-            open_head_next = closed_tail.next.next
+            # open_head_next = closed_tail.next.next
 
             # find position
             ptr_before = dummy_head
-            ptr = dummy_head.next
-            while(ptr):
+            ptr = dummy_head.next # error using ptr = head
+
+            # WHILE BEFORE IF THUS INCREASING TIME COMPLEXITY
+            while(ptr_before):
                 if comparator(ptr, open_head)>0:
                     ptr_before.next = open_head
+                    closed_tail.next = open_head.next
                     open_head.next = ptr
 
-                    closed_tail.next = open_head_next
+                    # closed_tail.next = open_head_next
                     break
 
                 if ptr==open_head:
@@ -45,9 +51,42 @@ class Solution:
         return dummy_head.next
 
 
+    def insertionSortList(self, head):
+        """
+        Know issue: Time Limit Excedded
+        """
+        comparator = lambda x, y: cmp(x.val, y.val)
+        # open set & closed set
+        # iterate through all the nodes
+        dummy_head = ListNode(0)
+        dummy_head.next = head
+
+        closed_tail = head
+        while (closed_tail and closed_tail.next):
+            open_head = closed_tail.next
+            open_head_next = closed_tail.next.next
+            if not comparator(closed_tail, open_head)<=0: # only compare the closed set tail and open set head
+
+                pre = dummy_head
+                while comparator(pre.next, open_head)<0: # find position 
+                    pre = pre.next
+
+                # swap nodes
+                open_head.next = pre.next
+                pre.next = open_head
+                closed_tail.next = open_head_next
+
+
+
+            else:
+                closed_tail = closed_tail.next
+
+
+        return dummy_head.next
+
 if __name__=="__main__":
     import random
-    lst = [ListNode(i) for i in random.sample(xrange(100), 10)]
+    lst = [ListNode(i) for i in random.sample(xrange(-1000, 1000), 1000)]
     # lst = [ListNode(1), ListNode(3), ListNode(2)]
     # lst = [ListNode(i) for i in range(10, -1, -1)]
     for i in range(len(lst)):
@@ -60,9 +99,9 @@ if __name__=="__main__":
 
     head = Solution().insertionSortList(lst[0])
     current = head
-    for i in range(10):
+    for i in range(len(lst)):
         print current.val
         current = current.next
-        pass
+
 
 
