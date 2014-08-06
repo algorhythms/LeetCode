5d3f2f3eae3542346a8859bf1cc6fc7803d3e609
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-08-06 19:15:25 +0800

    83

---
+++ 083_remove-duplicates-from-sorted-list.py
@@ -0,0 +1,38 @@
+__author__ = 'Danyang'
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def deleteDuplicates(self, head):
+        """
+        Two pointers, closed_ptr and open_ptr
+        O(n)
+        """
+        # trivial
+        if not head:
+            return head
+        # main
+        closed_ptr = head
+        open_ptr = head.next
+        while open_ptr:
+            # find the non-duplicate
+            while open_ptr and closed_ptr.val==open_ptr.val:
+                open_ptr = open_ptr.next
+
+            closed_ptr.next = open_ptr
+            closed_ptr = closed_ptr.next
+            open_ptr = open_ptr.next if open_ptr else None
+
+        return head
+
+if __name__=="__main__":
+    nodes = [ListNode(1) for _ in range(2)]
+    for i in range(len(nodes)-1):
+        nodes[i].next = nodes[i+1]
+
+    Solution().deleteDuplicates(nodes[0])
