9423558c1cb9558365229cf785e57e21c077b06e
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-08-17 00:33:16 +0800

    108, 109

---
+++ 108 Convert Sorted Array to Binary Search Tree.py
@@ -0,0 +1,28 @@
+__author__ = 'Danyang'
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, num):
+        """
+        post-order, dfs
+        :param num: list of integers
+        :return: TreeNode
+        """
+        if not num:
+            return None
+
+        mid = (0+len(num))/2
+        left_subtree = self.sortedArrayToBST(num[0:mid])
+        right_subtree = self.sortedArrayToBST(num[mid+1:])
+
+        root = TreeNode(num[mid])
+        root.left = left_subtree
+        root.right = right_subtree
+
+        return root
+
---
+++ 109 Convert Sorted List to Binary Search Tree.py
@@ -0,0 +1,52 @@
+__author__ = 'Danyang'
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # class attribute to keep trace the currently processing nodes
+    h = None 
+
+    def sortedListToBST(self, head):
+        """
+        dfs
+        No O(1) access.
+        Bottom-up construction
+        :param head: ListNode
+        :return: TreeNode
+        """
+        if not head:
+            return head
+
+        self.h = head
+        length = self.getLength(head)
+        return self.sortedListToBST_dfs(0, length-1)
+
+    def sortedListToBST_dfs(self, start, end):
+        if start>end:
+            return
+        mid = (start+end)/2
+        left_subtree = self.sortedListToBST_dfs(start, mid-1)
+        root = TreeNode(self.h.val)
+        self.h = self.h.next
+        right_subtree = self.sortedListToBST_dfs(mid+1, end)
+
+        root.left = left_subtree
+        root.right = right_subtree
+        return root
+
+    def getLength(self, head):
+        count = 0
+        while head:
+            head = head.next
+            count += 1
+        return count
