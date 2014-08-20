20a3d3efb5f370e6fad1abf8ba8930bceab2a3e9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-08-20 19:53:18 +0800

    047, 107

---
+++ 047 Rotate Image.py
@@ -0,0 +1,23 @@
+__author__ = 'Danyang'
+class Solution:
+    def rotate(self, matrix):
+        """
+        rotate matrix n*n
+        1. flip along the diagonal
+        2. flip along x-axis
+        :param matrix: a list of lists of integers
+        :return: a list of lists of integers
+        """
+        n = len(matrix)
+        for row in range(n):
+            for col in range(n-row):
+                matrix[row][col], matrix[n-1-col][n-1-row] = matrix[n-1-col][n-1-row], matrix[row][col]  # by diagonal
+        for row in range(n/2):
+            for col in range(n):
+                matrix[row][col], matrix[n-1-row][col] = matrix[n-1-row][col], matrix[row][col]  # by x-axis
+
+        return matrix
+
+
+
+
---
+++ 107 Binary Tree Level Order Traversal II.py
@@ -0,0 +1,43 @@
+__author__ = 'Danyang'
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        bfs
+        :param root: TreeNode
+        :return: Integers
+        """
+        if not root:
+            return []
+        result = []
+        next_level = [root]
+        while next_level:
+            current_level = next_level
+            result.insert(0, [element.val for element in current_level])  # current level, only difference with Binary Tree Level Order Traversal I
+
+            next_level = []
+            for element in current_level:
+                if element.left:
+                    next_level.append(element.left)
+                if element.right:
+                    next_level.append(element.right)
+        return result
+
+if __name__=="__main__":
+    Solution().levelOrderBottom(TreeNode(1))
+
+
+
+
+
+
+
+
+
+
