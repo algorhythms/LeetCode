1e952a97134ad9a7b39712feb397a272060ac894
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-09-05 20:36:27 +0800

    040

---
+++ 040 First Missing Positive.py
@@ -0,0 +1,32 @@
+__author__ = 'Danyang'
+class Solution:
+    def firstMissingPositive(self, A):
+        """
+        Bucket Sort without extra space
+        O(n)
+
+        Extra space is eliminated by using the array A itself as a storage
+        :param A: a list of integers
+        :return: an integer
+        """
+        if not A:
+            return 1
+
+        i = 0
+        length = len(A)
+        while i<length:
+            current = A[i]
+            if current<=0 or current>length or A[current-1]==current:  # out-of-range or in-place
+                i += 1
+            else:
+                A[current-1], A[i] = current, A[current-1]
+
+
+        for i in xrange(length):
+            if A[i]!=i+1:
+                return i+1
+        return A[-1]+1
+
+
+if __name__=="__main__":
+    assert Solution().firstMissingPositive([3,4,-1,1])==2
