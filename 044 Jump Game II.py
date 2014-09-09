d14d73ed6b8fbc4fba9eaae8e5687f163d5e67af
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-09-09 12:30:02 +0800

    044

---
+++ 044 Jump Game II.py
@@ -0,0 +1,70 @@
+"""
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last
+index.)
+"""
+__author__ = 'Danyang'
+class Solution:
+    def jump_TLE(self, A):
+        """
+        dfs
+        :param A: a list of integers
+        :return: integer, minimum number of jumps
+        """
+        if not A:
+            return 0
+
+        length = len(A)
+        counter = 0
+        dp = [[] for _ in A]
+
+
+        q = []
+        q.append(0)
+        while q:
+            current_level = q
+            q = []
+            for ind in current_level:
+                if ind>=length-1:
+                    return counter
+                for j in xrange(ind+1, ind+A[ind]+1):
+                    if j not in current_level:  # avoid duplicate
+                        q.append(j)
+            counter += 1
+        return counter
+
+    def jump(self, A):
+        """
+        Simplified dfs, use pointers to scan the array.
+        Algorithm: Two Pointers
+        :param A: a list of integers
+        :return: integer, minimum number of jumps
+        """
+        length = len(A)
+        counter = 0
+
+        start = 0
+        end = 1  # max reach [0, 1)
+        current_max = 0
+        while end<length:  # when end==length, it has already reached the last item 
+            for i in xrange(start, end):
+                current_max = max(current_max, A[i]+i)
+
+            counter += 1
+            start = end
+            end = current_max+1
+
+        return counter
+
+
+if __name__=="__main__":
+    print Solution().jump([2,3,1,1,4])
+    assert Solution().jump([2,3,1,1,4])==2
