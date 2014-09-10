7397b50d1a1e84b1b3ceee1a8c7a0b6c18bc51af
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-09-10 11:49:29 +0800

    027 068

---
+++ 027 Divide Two Integers.py
@@ -0,0 +1,34 @@
+__author__ = 'Danyang'
+class Solution:
+    def divide(self, dividend, divisor):
+        """
+        O(n lgn)
+        use concept of binary
+        :param dividend:
+        :param divisor:
+        :return: integer
+        """
+        # exceptions
+        if divisor==0 or dividend==0:
+            return 0
+
+        # handle signs
+        sign = 1 if dividend>=0 and divisor>=0 or dividend<0 and divisor<0 else -1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+        while dividend>=divisor:
+            current_result = 1
+            current = divisor  # write inner loop first
+            while current<=dividend:
+                current <<= 1
+                current_result <<= 1
+
+            dividend -= current>>1
+            result += current_result>>1
+
+        return sign*result
+
+if __name__=="__main__":
+    print Solution().divide(5, -1)
---
+++ 068 Simplify Path.py
@@ -0,0 +1,45 @@
+"""
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+click to show corner cases.
+
+Corner Cases:
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+"""
+__author__ = 'Danyang'
+class Solution:
+    def simplifyPath(self, path):
+        """
+
+        :param path: a string
+        :return: a string
+        """
+        path = path.split("/")
+        path = filter(lambda x: x not in ("", " ", "."), path)
+
+        for ind, val in enumerate(path):  # some unexpected error in memory
+            if val=="..":
+                path[ind] = "."
+
+                i = 1
+                while ind-i>=0 and path[ind-i]==".": i += 1
+                if ind-i>=0: path[ind-i] = "."  # avoid unexpected path[-1]
+
+        path = filter(lambda x: x not in (".",), path)
+
+        if not path:
+            return "/"
+
+        path = map(lambda x: "/"+x, path)
+        return "".join(path)
+
+if __name__=="__main__":
+    assert Solution().simplifyPath("/a/./b///../c/../././../d/..//../e/./f/./g/././//.//h///././/..///")=="/e/f/g"
+    assert Solution().simplifyPath("/a/./b/../../c/")=="/c"
+    assert Solution().simplifyPath("/../")=="/"
