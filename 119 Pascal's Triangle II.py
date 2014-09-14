212ddf28f52ebbd2eda18d3e676a4557d1eb8770
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-09-14 14:27:10 +0800

    documentation, triangle

--- 119 Pascal's Triangle II.py
+++ 119 Pascal's Triangle II.py
@@ -1,8 +1,20 @@
+"""
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+"""
 __author__ = 'Danyang'
 class Solution:
     def getRow(self, rowIndex):
         """
-        iteration
+        iteration.
+
+        Math
+
         indexing
         use only O(k) extra space
         :param rowIndex: integer
--- 120 Triangle.py
+++ 120 Triangle.py
@@ -1,3 +1,19 @@
+"""
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row
+below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+"""
 __author__ = 'Danyang'
 class Solution:
     def minimumTotal(self, triangle):
