1bff2045db9dbe938c12bace3350688155f0cca9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-09-24 00:32:47 +0800

    132 133

---
+++ 132 Palindrome Partitioning II.py
@@ -0,0 +1,215 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+"""
+__author__ = 'Danyang'
+class Solution:
+    def minCut_MLE(self, s):
+        """
+        bfs
+        :param s: str
+        :return: int
+        """
+        q = [[s]]
+        count = -1
+        while q:
+            # cur = q.pop(0)  # not directly pop
+            length = len(q)
+            count += 1
+            for cur_level in xrange(length):
+                cur = q[cur_level]
+                if all(self.is_palindrome(item) for item in cur):
+                    return count
+                # 1 cut
+                for ind, val in enumerate(cur):
+                    for i in xrange(1, len(val)):
+                        cut1 = val[:i]
+                        cut2 = val[i:]
+                        new_cur = list(cur)
+                        new_cur[ind] = cut1
+                        new_cur.insert(ind+1, cut2)
+                        q.append(new_cur)
+            q = q[length:]
+
+    def minCut_TLE(self, s):
+        """
+        dp
+
+        a   b   a   b   b   b   a   b   b   a   b   a
+                    i                       k
+        if s[i:k+1] is palindrome, #cut is 0; otherwise
+        cut s[i:k+1] into palindrome, the #cut:
+          cut the s[i:k+1] to two parts
+          cut the left part into palindrome, #cut is dp[i, j]
+          cut the right part into palindrome, #cut is dp[j+1, k+1]
+        find the minimum for above
+
+        dp[i, n+1] = min(dp[i, j]+dp[j, k+1]+1)
+
+        when drawing the matrix, you will find it difficult to construct it at one shot (especially, vertical line)
+
+        * another algorithm is dfs with global_min
+        * to tell s[i:k+1] whether it is palindrome can be optimized by dp
+        :param s: str
+        :return: int
+        """
+        if not s:
+            return 0
+
+        length = len(s)
+        dp = [[1<<32-1 for _ in xrange(length+1)] for _ in xrange(length+1)]
+        for i in xrange(length+1):
+            try:
+                dp[i][i] = 0
+                dp[i][i+1] = 0
+            except IndexError:
+                pass
+
+        for i in xrange(length, -1, -1):
+            for k in xrange(i, length+1):
+                if self.is_palindrome(s[i:k]):
+                    dp[i][k] = 0
+                else:
+                    dp[i][k] = min(1+dp[i][j]+dp[j][k] for j in xrange(i+1, k))
+
+        return dp[0][length]
+
+    def is_palindrome(self, s):
+        return s==s[::-1]
+
+    def minCut_TLE2(self, s):
+        """
+        dp
+
+        a   b   a   b   b   b   a   b   b   a   b   a
+                    i                       k
+        if s[i:k+1] is palindrome, #cut is 0; otherwise
+        cut s[i:k+1] into palindrome, the #cut:
+          cut the s[i:k+1] to two parts
+          cut the left part into palindrome, #cut is dp[i, j]
+          cut the right part into palindrome, #cut is dp[j+1, k+1]
+        find the minimum for above
+
+        dp[i, n+1] = min(dp[i, j]+dp[j, k+1]+1)
+
+        when drawing the matrix, you will find it difficult to construct it at one shot (especially, vertical line)
+
+
+        Determine s[i:k+1] is palindrome:
+        dp2[i, k+1] = dp2[i+1, k] && s[i]==s[k]
+
+        * another algorithm is dfs with global_min
+        * to tell s[i:k+1] whether it is palindrome can be optimized by dp
+        :param s: str
+        :return: int
+        """
+        if not s:
+            return 0
+
+        length = len(s)
+        # palindrome dp
+        dp2 = [[False for _ in xrange(length+1)] for _ in xrange(length+1)]
+        for i in xrange(length+1):
+            try:
+                dp2[i][i] = True
+                dp2[i][i+1] = True
+            except IndexError:
+                pass
+
+        for i in xrange(length, -1, -1):
+            for j in xrange(i+2, length+1):
+                try:
+                    dp2[i][j] = dp2[i+1][j-1] and s[i]==s[j-1]
+                except IndexError:
+                    dp2[i][j] = True
+
+
+        # min cut dp
+        dp = [[1<<32-1 for _ in xrange(length+1)] for _ in xrange(length+1)]
+        for i in xrange(length+1):
+            try:
+                dp[i][i] = 0
+                dp[i][i+1] = 0
+            except IndexError:
+                pass
+
+        for i in xrange(length, -1, -1):
+            for k in xrange(i, length+1):
+                if dp2[i][k]:
+                    dp[i][k] = 0
+                else:
+                    dp[i][k] = min(1+dp[i][j]+dp[j][k] for j in xrange(i+1, k))
+
+        return dp[0][length]
+
+
+    def minCut(self, s):
+        """
+        dp
+
+        a   b   a   b   b   b   a   b   b   a   b   a
+                    i                       k
+        if s[i:k+1] is palindrome, #cut is 0; otherwise
+        cut s[i:k+1] into palindrome, the #cut:
+          cut the s[i:k+1] to two parts
+          cut the left part into palindrome, #cut is dp[i, j]
+          cut the right part into palindrome, #cut is dp[j+1, k+1]
+        find the minimum for above
+
+        dp[i, n+1] = min(dp[i, j]+dp[j, k+1]+1)
+
+        when drawing the matrix, you will find it difficult to construct it at one shot (especially, vertical line)
+
+        To avoid TLE, use 1-d dp instead of 2-d dp
+        D[i] represents #cut for s[i:length+1]
+        if s[i:j] is palindrome and we need #cut for s[j:] is D[j], then
+        for minimum: D[i] = min(D[j+1]+1) for all j
+
+        To avoid TLE, use dp for determination of palindrome
+        Determine s[i:k+1] is palindrome:
+        dp2[i, k+1] = dp2[i+1, k] && s[i]==s[k]
+
+        * another algorithm is dfs with global_min
+        * to tell s[i:k+1] whether it is palindrome can be optimized by dp
+        :param s: str
+        :return: int
+        """
+        if not s:
+            return 0
+
+        length = len(s)
+        # palindrome dp
+        dp2 = [[False for _ in xrange(length+1)] for _ in xrange(length+1)]
+        for i in xrange(length+1):
+            try:
+                dp2[i][i] = True
+                dp2[i][i+1] = True
+            except IndexError:
+                pass
+
+        for i in xrange(length, -1, -1):
+            for j in xrange(i+2, length+1):
+                try:
+                    dp2[i][j] = dp2[i+1][j-1] and s[i]==s[j-1]
+                except IndexError:
+                    dp2[i][j] = True
+
+        # min cut dp
+        D = [length-i-1 for i in xrange(length)]  # max is all cut
+        for i in xrange(length-1, -1, -1):
+            if dp2[i][length]:
+                D[i] = 0
+            else:
+                for j in xrange(i+1, length):
+                    if dp2[i][j]:
+                        D[i] = min(D[i], D[j]+1)
+        return D[0]
+
+
+
+if __name__=="__main__":
+    assert Solution().minCut("apjesgpsxoeiokmqmfgvjslcjukbqxpsobyhjpbgdfruqdkeiszrlmtwgfxyfostpqczidfljwfbbrflkgdvtytbgqalguewnhvvmcgxboycffopmtmhtfizxkmeftcucxpobxmelmjtuzigsxnncxpaibgpuijwhankxbplpyejxmrrjgeoevqozwdtgospohznkoyzocjlracchjqnggbfeebmuvbicbvmpuleywrpzwsihivnrwtxcukwplgtobhgxukwrdlszfaiqxwjvrgxnsveedxseeyeykarqnjrtlaliyudpacctzizcftjlunlgnfwcqqxcqikocqffsjyurzwysfjmswvhbrmshjuzsgpwyubtfbnwajuvrfhlccvfwhxfqthkcwhatktymgxostjlztwdxritygbrbibdgkezvzajizxasjnrcjwzdfvdnwwqeyumkamhzoqhnqjfzwzbixclcxqrtniznemxeahfozp")==452
---
+++ 133 Clone Graph.py
@@ -0,0 +1,104 @@
+"""
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+"""
+__author__ = 'Danyang'
+# Definition for a undirected graph node
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+    def __repr__(self):
+        return repr(self.label)
+
+class Solution:
+    def cloneGraph_TLE(self, node):
+        """
+        dfs
+        :param node: UndirectedGraphNode
+        :return: UndirectedGraphNode
+        """
+        return self.clone_graph_visited(node, set())
+
+    def clone_graph_visited(self, node, visited_set):
+        """
+        post-order traversal
+        Time Limit Exceeded
+        :param node:
+        :param visited_set:
+        :return:
+        """
+        if not node:
+            return
+        visited_set.add(node)
+        neighbors_cloned = [self.clone_graph_visited(neighbor, set(visited_set)) for neighbor in node.neighbors if neighbor not in visited_set]
+        node_cloned = UndirectedGraphNode(node.label)
+        for neighbor_cloned in neighbors_cloned:
+            if neighbor_cloned not in visited_set:
+                neighbor_cloned.neighbors.append(node_cloned)
+        node_cloned.neighbors = neighbors_cloned
+        return node_cloned
+
+    def cloneGraph(self, node):
+        """
+        bfs with visited
+        similar to 138 Copy List with Random Pointer
+
+        copy_map: dict, 1. book keeping whether it is coped (visited); 2. reserve for being copied as neighbor, # shadow
+        q: list, queue of nodes whose the neighbors are to be copied
+
+        :param node: UndirectedGraphNode
+        :return: UndirectedGraphNode
+        """
+        if not node:
+            return
+
+        original2copy = {} # dict
+        q = [node]  # queue of nodes whose the neighbors are to be copied
+
+        clone = UndirectedGraphNode(node.label)
+        original2copy[node] = clone
+        while q:
+            cur = q.pop()
+            for neighbor in cur.neighbors:
+                if neighbor in original2copy:  # already copied
+                    original2copy[cur].neighbors.append(original2copy[neighbor])
+                else:
+                    q.append(neighbor)
+                    clone_neighbor = UndirectedGraphNode(neighbor.label)
+                    original2copy[neighbor] = clone_neighbor
+                    original2copy[cur].neighbors.append(original2copy[neighbor])
+
+        return original2copy[node]
+
+
+if __name__=="__main__":
+    lst = [UndirectedGraphNode(i+1) for i in range(3)]
+    for item in lst:
+        item.neighbors = list(lst)
+        item.neighbors.remove(item)
+    cloned = Solution().cloneGraph(lst[0])
+    assert cloned.neighbors[0].label in (2, 3)
+    assert cloned.neighbors[1].label in (2, 3)
+
+
