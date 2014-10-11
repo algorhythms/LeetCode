04ff01da36b55510b8da47094c7add1f9241c3f8
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-11 11:40:09 +0800

    072 doc

--- 072 Edit Distance.py
+++ 072 Edit Distance.py
@@ -19,26 +19,26 @@ class Solution:
          convert word1[:i-1] to word2[:j] is d[i-1][j-1], then deletes word1[i]
 
         int EditDistance(char s[1..m], char t[1..n])
-           // For all i and j, d[i,j] will hold the Levenshtein distance between
-           // the first i characters of s and the first j characters of t.
-           // Note that d has (m+1)  x(n+1) values.
+           # For all i and j, d[i,j] will hold the Levenshtein distance between
+           # the first i characters of s and the first j characters of t.
+           # Note that d has (m+1)  x(n+1) values.
            let d be a 2-d array of int with dimensions [0..m, 0..n]
 
            for i in [0..m]
-             d[i, 0] = i // the distance of any first string to an empty second string
+             d[i, 0] = i # the distance of any first string to an empty second string
            for j in [0..n]
-             d[0, j] = j // the distance of any second string to an empty first string
+             d[0, j] = j # the distance of any second string to an empty first string
 
            for j in [1..n]
              for i in [1..m]
                if s[i] = t[j] then
-                 d[i, j] = d[i-1, j-1]       // no operation required
+                 d[i, j] = d[i-1, j-1]       # no operation required
                else
                  d[i, j] = minimum of
                             (
-                              d[i-1, j] + 1,  // a deletion
-                              d[i, j-1] + 1,  // an insertion
-                              d[i-1, j-1] + 1 // a substitution
+                              d[i-1, j] + 1,  # a deletion     # convert a[:i-1] to b[:j], then delete the last item of a
+                              d[i, j-1] + 1,  # an insertion   # convert a[:i] to b[:j-1], then insert the last item of b into a
+                              d[i-1, j-1] + 1 # a substitution # convert a[:i-1] to b[:j-1], then substitute a's last item to b's
                             )
 
            return d[m,n]
