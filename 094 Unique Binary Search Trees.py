f649529e1c50f7d6e2f4c510c7a2c34d2b797f70
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-11 17:01:48 +0800

    dp solution

--- 094 Unique Binary Search Trees.py
+++ 094 Unique Binary Search Trees.py
@@ -12,7 +12,7 @@ Given n = 3, there are a total of 5 unique BST's.
 """
 __author__ = 'Danyang'
 class Solution:
-    def numTrees(self, n):
+    def numTrees_math(self, n):
         """
         number of unique binary search tree
         Catalan Number
@@ -31,5 +31,30 @@ class Solution:
             factorial *= i+1
         return factorial
 
+    def numTrees(self, n):
+        """
+        number of unique binary search tree
+        dp
+
+        dp[i] means # BST constructed from [1...i]
+
+        dp[3] = dp[0]*dp[2]  # 1 as pivot
+               +dp[1]*dp[1]  # 2 is pivot
+               +dp[2]*dp[0]  # 3 as pivot
+
+        :param n: integer
+        :return: integer
+        """
+        if n<2:
+            return n
+
+        dp = [0 for _ in xrange(n+1)]
+        dp[0] = 1
+        for i in xrange(1, n+1):
+            for j in xrange(i):
+                dp[i] += dp[j]*dp[i-j-1]
+        return dp[-1]
+
+
 if __name__=="__main__":
-    print Solution().numTrees(100)-Solution().numTrees(99)
+    assert Solution().numTrees(100)==Solution().numTrees_math(100)
