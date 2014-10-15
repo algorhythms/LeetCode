6b82747cf7a09f15c518b6cbb31c56191f5dc119
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-15 19:18:58 +0800

    question body added

--- 099 Recover Binary Search Tree.py
+++ 099 Recover Binary Search Tree.py
@@ -1,3 +1,12 @@
+"""
+Two elements of a binary search tree (BST) are swapped by mistake.
+
+Recover the tree without changing its structure.
+
+Note:
+A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+"""
 __author__ = 'Danyang'
 # Definition for a  binary tree node
 class TreeNode:
