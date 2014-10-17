57463ed68b4950a82aeeb839fcfcd7da8d3b3948
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2014-10-17 22:05:52 +0800

    added more solutions to illustrate bit manipulation.

--- 137 Single Number II.py
+++ 137 Single Number II.py
@@ -6,7 +6,7 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 """
 __author__ = 'Danyang'
 class Solution:
-    def singleNumber(self, A):
+    def singleNumber_optimal(self, A):
         """
         No extra memory, thus unable to use hash table, requiring constant space solution
 
@@ -33,3 +33,73 @@ class Solution:
             bit_1 = (bit_0 & element) | (bit_1 & ~element)
             bit_0 = (temp & element) | (bit_0 & ~element)
         return bit_1
+
+    def singleNumber_array(self, A):
+        """
+        add the bit vertically; bit sum
+
+        use int array
+        assume 32 bit, not applicable in Python for negative number
+
+        Note: possible to pull up the iteration over 32
+        :param A:
+        :return:
+        """
+        cnt = [0 for _ in xrange(32)]
+
+        for elmt in A:
+            for i in xrange(32):
+                if elmt>>i&1==1:
+                    cnt[i] = (cnt[i]+1)%3
+
+        result = 0
+        for i in xrange(32):
+            result |= cnt[i]<<i
+
+        return result
+
+
+    def singleNumber(self, A):
+        """
+        add the bit vertically; bit sum
+
+        use int array
+        assume 32 bit, not applicable in Python for negative number
+
+        Note: possible to pull up the iteration over 32
+        :param A:
+        :return:
+        """
+        one, two, three = 0, 0, 0
+
+        # for elmt in A:
+        #     three |= two&elmt
+        #     two^= elmt
+        #
+        #     two |= one&elmt  # add to two
+        #     one ^= elmt  # del elmt appear twice
+
+        for elmt in A:
+            # after processing elmt
+            two |= one&elmt
+            one ^= elmt
+            three = one&two
+
+            one &= ~three
+            two &= ~three
+        return one
+
+
+
+
+
+
+if __name__=="__main__":
+    # possible negative numbers
+    tests = [
+        [1, 1, 1, 2, 2, 2, 3, 4, 4, 4],
+        [1]
+    ]
+    for A in tests:
+        assert Solution().singleNumber_optimal(A)==Solution().singleNumber_array(A)
+        assert Solution().singleNumber_optimal(A)==Solution().singleNumber(A)
