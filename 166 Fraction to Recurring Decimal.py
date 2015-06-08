0f93e17a19762e0f9a5cce8c39715ba3a0b1d3e0
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-06-08 11:07:16 +0800

    166

--- 166 Fraction to Recurring Decimal.py
+++ 166 Fraction to Recurring Decimal.py
@@ -15,10 +15,52 @@ __author__ = 'Daniel'
 class Solution:
     def fractionToDecimal(self, numerator, denominator):
         """
+        The key is the remainder
         :type numerator: int
         :type denominator: int
         :rtype: str
         """
+        sign = 1 if numerator*denominator >= 0 else -1
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+
+        int_part = numerator/denominator
+        frac_part = numerator-int_part*denominator
+
+        if frac_part:
+            decimal_part = self.frac(numerator-int_part*denominator, denominator)
+            ret = str(int_part)+"."+decimal_part
+        else:
+            ret = str(int_part)
+
+        if sign < 0:
+            ret = "-" + ret
+
+        return ret
+
+    def frac(self, num, deno):
+        """
+        real fraction part
+        """
+        ret = []
+        d = {}
+        i = 0
+
+        while num:
+            num *= 10
+            q = num/deno
+            r = num%deno
+            if (q, r) in d:
+                ret.append(")")
+                ret.insert(d[(q, r)], "(")
+                return "".join(ret)
+
+            ret.append(str(q))
+            d[(q, r)] = i
+            i += 1
+            num -= q*deno
+
+        return "".join(ret)
 
 
 class Solution_error:
@@ -70,8 +112,7 @@ class Solution_error:
         return "".join(ret)
 
 if __name__ == "__main__":
-    print Solution().fractionToDecimal(1, 333)
-    print Solution().fractionToDecimal(1, 90)
-
-
-
+    assert Solution().fractionToDecimal(1, 333) == "0.(003)"
+    assert Solution().fractionToDecimal(1, 90) == "0.0(1)"
+    assert Solution().fractionToDecimal(-50, 8) == "-6.25"
+    assert Solution().fractionToDecimal(7, -12) == "-0.58(3)"
