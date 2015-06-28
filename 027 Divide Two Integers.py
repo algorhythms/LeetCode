c3984c17c2df6bfda34b91d6f905b6efea9f92d8
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-06-28 15:53:16 +0800

    fix #4

--- 027 Divide Two Integers.py
+++ 027 Divide Two Integers.py
@@ -2,6 +2,10 @@
 Divide two integers without using multiplication, division and mod operator.
 """
 __author__ = 'Danyang'
+MAX_INT = 2147483647
+MIN_INT = -2147483648
+
+
 class Solution:
     def divide(self, dividend, divisor):
         """
@@ -13,19 +17,22 @@ class Solution:
         :return: integer
         """
         # exceptions
-        if divisor==0 or dividend==0:
+        if divisor == 0 or dividend == 0:
             return 0
 
+        if dividend == MIN_INT and divisor == -1:
+            return MAX_INT
+
         # handle signs
-        sign = 1 if dividend>=0 and divisor>=0 or dividend<0 and divisor<0 else -1
+        sign = 1 if dividend >= 0 and divisor >= 0 or dividend < 0 and divisor < 0 else -1
         dividend = abs(dividend)
         divisor = abs(divisor)
 
         result = 0
-        while dividend>=divisor:
+        while dividend >= divisor:
             current_result = 1
             current = divisor  # write inner loop first
-            while current<=dividend:
+            while current <= dividend:
                 current <<= 1
                 current_result <<= 1
 
@@ -34,5 +41,6 @@ class Solution:
 
         return sign*result
 
-if __name__=="__main__":
-    print Solution().divide(5, -1)
+
+if __name__ == "__main__":
+    assert Solution().divide(5, -1) == -5
