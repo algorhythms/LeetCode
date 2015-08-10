9837a49dc8bed42b27fe4bef85f2286fd262e912
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-11 00:02:27 +0800

    format

--- 251 Flatten 2D Vector.py
+++ 251 Flatten 2D Vector.py
@@ -15,7 +15,6 @@ class Vector2D:
         self.i = 0
         self.j = 0
 
-
     def next(self):
         """
 
@@ -37,6 +36,5 @@ class Vector2D:
         while self.i < len(self.vec2d) and self.j >= len(self.vec2d[self.i]):
             self.i += 1
             self.j = 0
-
 
         return self.i < len(self.vec2d) and self.j < len(self.vec2d[self.i])
