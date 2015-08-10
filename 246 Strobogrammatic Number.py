35064d52f9344494330261cd59da2e8d33f8bfdb
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-11 00:03:09 +0800

    strobogrammatic

---
+++ 246 Strobogrammatic Number.py
@@ -0,0 +1,32 @@
+"""
+Premium Question
+"""
+__author__ = 'Daniel'
+
+
+class Solution:
+    def __init__(self):
+        self.map = {
+            "1": "1",
+            "6": "9",
+            "9": "6",
+            "8": "8",
+            "0": "0"
+        }
+
+
+    def isStrobogrammatic(self, num):
+        """
+
+        :type num: str
+        :rtype: bool
+        """
+        num = list(num)
+        rev = []
+        for digit in reversed(num):
+            try:
+                rev.append(self.map[digit])
+            except KeyError:
+                return False
+
+        return num == rev
