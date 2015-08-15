d1a31f28857946bd9daebb482a7cd7c960127093
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-16 01:29:09 +0800

    preorder of bin tree

---
+++ 255 Verify Preorder Sequence in Binary Search Tree.py
@@ -0,0 +1,30 @@
+"""
+Premium Question
+"""
+__author__ = 'Daniel'
+
+
+class Solution:
+    def verifyPreorder(self, preorder):
+        """
+
+        :type preorder:list[int]
+        :rtype: bool
+        """
+        left_finished = None
+        stk = []
+        for num in preorder:
+            if left_finished and num < left_finished:
+                return False
+
+            while stk and stk[-1] < num:
+                left_finished = stk.pop()
+
+            stk.append(num)
+
+        return True
+
+if __name__ == "__main__":
+    preorder = [3, 5, 2, 1, 4, 7, 6, 9, 8, 10]
+    assert Solution().verifyPreorder(preorder) == False
+
