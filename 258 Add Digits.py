807fb2d77b8871806937be420997cea4c4a817ee
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-16 22:07:22 +0800

    number

---
+++ 258 Add Digits.py
@@ -0,0 +1,30 @@
+"""
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+"""
+__author__ = 'Daniel'
+
+
+class Solution:
+    def addDigits(self, num):
+        """
+        Enumerate and find the pattern
+
+        Prove pattern:
+        num = a0 * 10^0 + a1*10^1 + a2*10^2 ...
+        addDigits(num) = a0 + a1 + a2 ... = num%9
+
+        :type num:
+        :rtype: int
+        """
+        digit = num % 9
+        if digit == 0 and num != 0:
+            return 9
+        else:
+            return digit
