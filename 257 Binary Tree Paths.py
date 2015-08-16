1b05a7bff5104520c0e6e0bd70d738d446e9a5fc
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-16 21:58:33 +0800

    tree

---
+++ 257 Binary Tree Paths.py
@@ -0,0 +1,56 @@
+"""
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+
+"""
+__author__ = 'Daniel'
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+
+        :type root: TreeNode
+        :rtype: list[str]
+        """
+        if not root:
+            return []
+
+        ret = []
+        self.dfs(root, [], ret)
+        return ret
+
+    def dfs(self, cur, path, ret):
+        """
+        pre-check
+        """
+        path.append(cur)
+        if not cur.left and not cur.right:
+            ret.append("->".join(map(lambda x: str(x.val), path)))
+            return
+
+        if cur.left:
+            self.dfs(cur.left, path, ret)
+            path.pop()  # pop the shared path
+
+        if cur.right:
+            self.dfs(cur.right, path, ret)
+            path.pop()  # pop the shared path
+
