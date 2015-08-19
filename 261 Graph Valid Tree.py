901d31d5cc92fc88e8ff468f5d8551cb60dc96bd
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-19 23:46:28 +0800

    init

---
+++ 261 Graph Valid Tree.py
@@ -0,0 +1,13 @@
+"""
+Premium Question
+"""
+__author__ = 'Daniel'
+
+
+class Solution:
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :edges: List[List[int]
+        :rtype: bool
+        """
---
+++ 263 Ugly Number.py
@@ -0,0 +1,18 @@
+"""
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not
+ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number.
+"""
+__author__ = 'Daniel'
+
+
+class Solution:
+    def isUgly(self, num):
+        """
+
+        :type num: int
+        :rtype: bool
+        """
