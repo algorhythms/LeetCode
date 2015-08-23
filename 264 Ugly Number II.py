9a2458523dee338db26b892c13be2a637b49af30
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-24 00:56:20 +0800

    heap

--- 264 Ugly Number II.py
+++ 264 Ugly Number II.py
@@ -6,12 +6,51 @@ is the sequence of the first 10 ugly numbers.
 
 Note that 1 is typically treated as an ugly number.
 """
+import heapq
+
+
 __author__ = 'Daniel'
 
 
-class Solution:
+class Node(object):
+    def __init__(self, origin, q):
+        self.origin = origin
+        self.q = q
+
+    def __cmp__(self, other):
+        return self.q[0] - other.q[0]
+
+
+class Solution(object):
     def nthUglyNumber(self, n):
         """
+        Heap
         :type n: int
         :rtype: int
         """
+        if n == 1:
+            return 1
+
+        n -= 1  # exclude 1
+
+        ugly = [2, 3, 5]
+        qs = [Node(i, [i]) for i in ugly]
+        h = list(qs)  # shallow copy
+
+        heapq.heapify(h)
+
+        cnt = 0
+        ret = 2
+        while cnt < n:
+            cnt += 1
+            popped = heapq.heappop(h)
+            ret = popped.q.pop(0)
+            for i in xrange(ugly.index(popped.origin), 3):
+                qs[i].q.append(ret*ugly[i])
+
+            heapq.heappush(h, popped)
+
+        return ret
+
+if __name__ == "__main__":
+    assert Solution().nthUglyNumber(10) == 12
