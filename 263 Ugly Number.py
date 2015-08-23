83ba9fde098d2572a6b6197cbba9874a472211e8
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-24 00:27:41 +0800

    prime factor

--- 263 Ugly Number.py
+++ 263 Ugly Number.py
@@ -9,10 +9,31 @@ Note that 1 is typically treated as an ugly number.
 __author__ = 'Daniel'
 
 
-class Solution:
+class Solution(object):
     def isUgly(self, num):
         """
+        Prime factor
 
         :type num: int
         :rtype: bool
         """
+        if num < 1:
+            return False
+        if num == 1:
+            return True
+
+        ugly = {2, 3, 5}
+
+        prime = 2
+        while prime*prime <= num and num > 1:
+            if num % prime != 0:
+                prime += 1
+            else:
+                num /= prime
+                if prime not in ugly:
+                    return False
+
+        if num not in ugly:
+            return False
+
+        return True
