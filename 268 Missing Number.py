1d0ff35ded055c7f1c0262a0067c9de366cbe5b6
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-28 00:13:14 +0800

    doc

--- 268 Missing Number.py
+++ 268 Missing Number.py
@@ -14,13 +14,21 @@ __author__ = 'Daniel'
 class Solution(object):
     def missingNumber(self, nums):
         """
-        Algorithm: 
+        Algorithm:
         Hashmap but use the array itself as the hashmap
 
         Notice:
-        nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+            nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
         Above is wrong, since evaluate from left to right; thus, when nums[nums[i]] on RHS is None, on LHS, nums[i] is
         eval to None and nums[nums[i]] indexes by None, causing errors.
+
+        Alternatively, it is correct that:
+            nums[nums[i]], nums[i]= nums[i], nums[nums[i]]
+
+        To be safe, just use:
+            j = nums[i]
+            nums[i], nums[j] = nums[j], nums[i]
+
         :type nums: List[int]
         :rtype: int
         """
