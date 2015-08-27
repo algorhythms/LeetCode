e24e0a906383cf496db51ca84d10eafa15e43ed1
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-08-28 01:15:47 +0800

    map

---
+++ 266 Palindrome Permutation.py
@@ -0,0 +1,26 @@
+"""
+Premium Question
+"""
+from collections import defaultdict
+
+__author__ = 'Daniel'
+
+
+class Solution(object):
+    def canPermutePalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        m = defaultdict(int)
+        for c in s:
+            m[c] += 1
+
+        once = False
+        for v in m.values():
+            if v % 2 == 1:
+                if once:
+                    return False
+                once = True
+
+        return True
