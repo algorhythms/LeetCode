9f71d714a3cefdf203e6c2aaa4051eeaf79ab341
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-09-03 00:41:13 +0800

    update 2 algorithms

--- 271 Encode and Decode Strings.py
+++ 271 Encode and Decode Strings.py
@@ -9,6 +9,64 @@ class Codec(object):
         """
         Encodes a list of strings to a single string.
 
+        Algorithm: Length info
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        strs = map(lambda x: str(len(x))+"/"+x, strs)
+        return reduce(lambda x, y: x+y, strs, "")  # i.e. "".join(strs)
+
+    def decode(self, s):
+        """
+        Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        strs = []
+        i = 0
+        while i < len(s):
+            j = s.index("/", i)
+            l = int(s[i:j])
+            strs.append(s[j+1:j+1+l])
+            i = j+1+l
+
+        return strs
+
+
+class Codec(object):
+    def encode(self, strs):
+        """
+        Encodes a list of strings to a single string.
+
+        Algorithm: Escape
+
+        :type strs: List[str]
+        :rtype: str
+        """
+        strs = map(lambda x: x.replace("\n", "\n\n")+"_\n_", strs)
+        return reduce(lambda x, y: x+y, strs, "")
+
+    def decode(self, s):
+        """
+        Decodes a single string to a list of strings.
+
+        :type s: str
+        :rtype: List[str]
+        """
+        strs = s.split("_\n_")
+        strs = strs[:-1]  # clear the trailing delimiter
+        return map(lambda x: x.replace("\n\n", "\n"), strs)
+
+
+class Codec_error(object):
+    def encode(self, strs):
+        """
+        Encodes a list of strings to a single string.
+
+        This algorithm contains bugs if \\x00 exits in the original string  
+
         :type strs: List[str]
         :rtype: str
         """
