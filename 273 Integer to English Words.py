5aa1dbbf80033761ab7674e59214cc62d0c8c097
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-09-13 00:21:31 -0700

    math

---
+++ 273 Integer to English Words.py
@@ -0,0 +1,89 @@
+"""
+Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 2^31 - 1.
+
+For example,
+123 -> "One Hundred Twenty Three"
+12345 -> "Twelve Thousand Three Hundred Forty Five"
+1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+"""
+__author__ = 'Daniel'
+
+
+class Solution(object):
+    def __init__(self):
+        self.m = {
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            11: "Eleven",
+            12: "Twelve",
+            13: "Thirteen",
+            14: "Fourteen",
+            15: "Fifteen",
+            16: "Sixteen",
+            17: "Seventeen",
+            18: "Eighteen",
+            19: "Nineteen",
+            20: "Twenty",
+            30: "Thirty",
+            40: "Forty",
+            50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty",
+            90: "Ninety",
+            100: "Hundred",
+            1000: "Thousand",
+            1000000: "Million",
+            1000000000: "Billion"
+
+        }
+
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return "Zero"
+
+        ret = []
+        self.toWords(num, ret)
+        return " ".join(map(str, ret))
+
+    def toWords(self, num, ret):
+        sigs = [1000000000, 1000000, 1000, 100]
+        for sig in sigs:
+            num = self.partial(num, sig, ret)
+
+        ten = 10
+        if num/ten > 1:
+            ret.append(self.m[num/ten*ten])
+            if num%ten != 0:
+                ret.append(self.m[num%ten])
+        elif num/ten == 1:
+            ret.append(self.m[num])
+        elif num != 0:
+            ret.append(self.m[num])
+
+    def partial(self, num, sig, ret):
+        if num/sig:
+            pre = []
+            self.toWords(num/sig, pre)
+            ret.extend(pre)
+            ret.append(self.m[sig])
+            num %= sig
+
+        return num
+
+
+if __name__ == "__main__":
+    assert Solution().numberToWords(1234567891) == "One Billion Two Hundred Thirty Four Million Five Hundred Sixty " \
+                                                   "Seven Thousand Eight Hundred Ninety One"
