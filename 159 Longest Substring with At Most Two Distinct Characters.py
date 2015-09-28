7365e4cf8144f0106845ba8359c7fcb8606ed2d3
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-09-28 16:02:15 -0700

    sliding window

---
+++ 159 Longest Substring with At Most Two Distinct Characters.py
@@ -0,0 +1,35 @@
+"""
+Premium Question
+"""
+from collections import defaultdict
+
+__author__ = 'Daniel'
+
+
+class Solution(object):
+    def lengthOfLongestSubstringTwoDistinct(self, s):
+        """
+        Sliding Window
+        :type s: str
+        :rtype: int
+        """
+        m = defaultdict(int)
+        i = 0
+        j = 0
+        maxa = 0
+        for j in xrange(len(s)):
+            m[s[j]] += 1
+            while len(m) > 2:
+                m[s[i]] -= 1
+                if m[s[i]] == 0:
+                    del m[s[i]]
+
+                i += 1
+
+            maxa = max(maxa, j-i+1)
+
+        return maxa
+
+
+if __name__ == "__main__":
+    print Solution().lengthOfLongestSubstringTwoDistinct("ecebaaaaaacdbb")
