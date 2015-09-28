15c2cf133e0c3656adbbbd96ae06b02cda1449f9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-09-28 16:10:22 -0700

    bst

---
+++ 285 Inorder Successor in BST.py
@@ -0,0 +1,34 @@
+"""
+Premium Question
+"""
+__author__ = 'Daniel'
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        search
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        find = [None]
+        self.search(root, p, find)
+        return find[0]
+
+    def search(self, cur, p, find):
+        if not cur:
+            return
+
+        if cur.val > p.val:
+            find[0] = cur
+            self.search(cur.left, p, find)
+        else:
+            self.search(cur.right, p, find)
