e839b36fb3cf611f58ed36d7d5ee455fbd858e44
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-10-15 20:54:40 -0700

    update

--- 065 Plus One.py
+++ 065 Plus One.py
@@ -4,7 +4,9 @@ Given a non-negative number represented as an array of digits, plus one to the n
 The digits are stored such that the most significant digit is at the head of the list.
 """
 __author__ = 'Danyang'
-class Solution:
+
+
+class Solution(object):
     def plusOne(self, digits):
         """
         Math
@@ -13,20 +15,15 @@ class Solution:
         :param digits: a list of integer digits
         :return: a list of integer digits
         """
-        cur = len(digits)-1
-        while True:
-            if cur>=0:
-                digits[cur] += 1
-                if digits[cur]<10:
-                    break
-                else:
-                    digits[cur] -= 10
-                    cur -= 1
+        for i in xrange(len(digits)-1, -1, -1):
+            digits[i] += 1
+            if digits[i] < 10:
+                return digits
             else:
-                # MSB
-                digits.insert(0, 1)
-                break
+                digits[i] -= 10
 
+        # MSB
+        digits.insert(0, 1)
         return digits
 
     def plusOne(self, digits):
@@ -41,7 +38,7 @@ class Solution:
         carry = 0
         for i in xrange(len(digits)):  # for ind, val in enumerate(digits):
             digits[i] += carry
-            if digits[i]>9:
+            if digits[i] > 9:
                 digits[i] -= 10
                 carry = 1
             else:
@@ -54,6 +51,7 @@ class Solution:
         digits.reverse()
         return digits
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     digits = [9]
-    assert Solution().plusOne(digits)==[1, 0]
+    assert Solution().plusOne(digits) == [1, 0]
