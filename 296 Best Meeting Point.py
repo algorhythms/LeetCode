8983ba9dc664785713fec3cbd53991a9c4483af2
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-10-21 20:04:45 -0700

    mahanttan distance

--- 296 Best Meeting Point.py
+++ 296 Best Meeting Point.py
@@ -6,7 +6,7 @@ __author__ = 'Daniel'
 
 
 class Solution(object):
-    def minTotalDistance(self, grid):
+    def minTotalDistance_3lines(self, grid):
         x = sorted([i for i, row in enumerate(grid) for v in row if v == 1])
         y = sorted([j for row in grid for j, v in enumerate(row) if v == 1])
         return sum([abs(x[len(x)/2]-i)+abs(y[len(y)/2]-j) for i, row in enumerate(grid) for j, v in enumerate(row) if v == 1])
