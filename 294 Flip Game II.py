b7f556b03a5c09fad23c919224f439d847d7a199
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-10-22 23:55:34 -0700

    memoization

--- 294 Flip Game II.py
+++ 294 Flip Game II.py
@@ -6,17 +6,37 @@ __author__ = 'Daniel'
 
 
 class Solution(object):
+    def __init__(self):
+        self.d = {}
+
     def canWin(self, s):
-        return any(not self.canWin(s[:i]+"--"+s[i+2:]) for i in xrange(len(s)-1) if s[i:i+2] == "++")
+        """
+        memoization
+        110ms
+        """
+        if s not in self.d:
+            flag = False
+            for i in xrange(len(s)-1):
+                if s[i:i+2] == "++":
+                    if not self.canWin(s[:i]+"--"+s[i+2:]):
+                        flag = True
+                        break
+            self.d[s] = flag
+
+        return self.d[s]
+
+    def canWin_oneline(self, s):
+        return any(not self.canWin_oneline(s[:i]+"--"+s[i+2:]) for i in xrange(len(s)-1) if s[i:i+2] == "++")
 
     def canWin_trivial(self, s):
         """
+        3200 ms
         :type s: str
         :rtype: bool
         """
         for i in xrange(len(s)-1):
             if s[i:i+2] == "++":
-                if not self.canWin(s[:i]+"--"+s[i+2:]):
+                if not self.canWin_trivial(s[:i]+"--"+s[i+2:]):
                     return True
 
         return False
