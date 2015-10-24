4eee849a0f9f0324cda0e40279e9d70a091dc845
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-10-23 21:54:03 -0700

    update reversing linked list

--- 206 Reverse Linked List.py
+++ 206 Reverse Linked List.py
@@ -4,13 +4,13 @@ Reverse a singly linked list.
 __author__ = 'Daniel'
 
 
-class ListNode:
+class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
-class Solution:
+class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
@@ -26,6 +26,8 @@ class Solution:
         cur = pre.next
         while pre and cur:
             pre, cur.next, cur = cur, pre, cur.next
+            # incorrect evaluation order
+            # pre, cur, cur.next = cur, cur.next, pre
 
         dummy.next.next = None  # original head
         return pre  # new head
