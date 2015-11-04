d9ec458309b19c8cf2089bf930daee6390d8e555
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-11-04 01:18:16 -0800

    LIS

--- 300 Longest Increasing Subsequence.py
+++ 300 Longest Increasing Subsequence.py
@@ -32,12 +32,12 @@ class Solution(object):
                 l += 1
                 M[l] = i
             else:
-                j = self.bin_saerch(M, A, A[i], 1, l+1)
+                j = self.bin_search(M, A, A[i], 1, l+1)
                 M[j] = i
 
         return l
 
-    def bin_saerch(self, M, A, t, lo=0, hi=None):
+    def bin_search(self, M, A, t, lo=0, hi=None):
         if not hi: hi = len(M)
         while lo < hi:
             m = (lo+hi)/2
@@ -49,6 +49,44 @@ class Solution(object):
                 hi = m
 
         return lo
+
+    def lengthOfLIS_output_all(self, A):
+        """
+        Maintain the result of LIS
+        M: min of index last value of LIS of a particular length
+        R: result table, store the predecessor idx
+        :type A: List[int]
+        :rtype: int
+        """
+        if not A:
+            return 0
+
+        n = len(A)
+        M = [-1 for _ in xrange(n+1)]
+        R = [-1 for _ in xrange(n)]
+        l = 1
+        M[l] = 0
+        for i in xrange(1, n):
+            if A[i] > A[M[l]]:
+                l += 1
+                M[l] = i
+                R[i] = M[l-1]
+            else:
+                j = self.bin_search(M, A, A[i], 1, l+1)
+                M[j] = i
+                R[i] = M[j-1] if j-1 >= 1 else -1
+
+        cur = M[l]
+        ret = []
+        while True:
+            ret.append(A[cur])
+            if R[cur] == -1: break
+            cur = R[cur]
+
+        ret = ret[::-1]
+        print ret
+
+        return l
 
     def lengthOfLIS_dp(self, A):
         """
