2c206dc1e5a77c3fb8b9e541727703910d47ca0b
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-11-03 23:41:08 -0800

    LIS

---
+++ 300 Longest Increasing Subsequence.py
@@ -0,0 +1,83 @@
+"""
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+For example,
+Given [10, 9, 2, 5, 3, 7, 101, 18],
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one
+LIS combination, it is only necessary for you to return the length.
+
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+"""
+__author__ = 'Daniel'
+
+
+class Solution(object):
+    def lengthOfLIS(self, A):
+        """
+        M: min of index last value of LIS of a particular length
+        :type A: List[int]
+        :rtype: int
+        """
+        if not A:
+            return 0
+
+        n = len(A)
+        M = [-1 for _ in xrange(n+1)]
+        l = 1
+        M[l] = 0
+        for i in xrange(1,n):
+            if A[i] > A[M[l]]:
+                l += 1
+                M[l] = i
+            else:
+                j = self.bin_saerch(M, A, A[i], 1, l+1)
+                M[j] = i
+
+        return l
+
+    def bin_saerch(self, M, A, t, lo=0, hi=None):
+        if not hi: hi = len(M)
+        while lo < hi:
+            m = (lo+hi)/2
+            if A[M[m]] == t:
+                return m
+            elif A[M[m]] < t:
+                lo = m + 1
+            else:
+                hi = m
+
+        return lo
+
+    def lengthOfLIS_dp(self, A):
+        """
+        dp
+
+        let F[i] be the LIS length ends at A[i]
+        F[i] = max(F[j]+1 for all j < i if A[i] > A[j])
+
+        avoid max() arg is an empty sequence
+
+        O(n^2)
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not A:
+            return 0
+
+        n = len(A)
+        F = [1 for _ in xrange(n)]
+        maxa = 1
+        for i in xrange(1, n):
+            F[i] = max(
+                F[j] + 1 if A[i] > A[j] else 1
+                for j in xrange(i)
+            )
+            maxa = max(maxa, F[i])
+
+        return maxa
+
+
+if __name__ == "__main__":
+    print Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])
