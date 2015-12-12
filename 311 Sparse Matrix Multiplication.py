5d6eab69ef8a8f734d9ef7d39c6fd3951c5b90a8
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-12-12 14:44:43 -0800

    posting list

---
+++ 311 Sparse Matrix Multiplication.py
@@ -0,0 +1,74 @@
+"""
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+"""
+__author__ = 'Daniel'
+
+
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        Brute force O(n^3)
+        O(n^2 k)
+        Posting list
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n = len(A), len(A[0])
+        A1 = [{} for _ in xrange(m)]
+        for i in xrange(m):
+            for j in xrange(n):
+                if A[i][j] != 0:
+                    A1[i][j] = A[i][j]
+
+        m, n = len(B), len(B[0])
+        B1 = [{} for _ in xrange(n)]
+        for i in xrange(m):
+            for j in xrange(n):
+                if B[i][j] != 0:
+                    B1[j][i] = B[i][j]
+
+        ret = [[0 for _ in xrange(len(B[0]))] for _ in xrange(len(A))]
+        for i, row in enumerate(A1):
+            for j, col in enumerate(B1):
+                s = 0
+                for k in row.keys():
+                    if k in col:
+                        s += row[k]*col[k]
+                ret[i][j] = s
+
+        return ret
+
+if __name__ == "__main__":
+    A = [
+        [1, 0, 0],
+        [-1, 0, 3]
+    ]
+
+    B = [
+        [7, 0, 0],
+        [0, 0, 0],
+        [0, 0, 1]
+    ]
+    assert Solution().multiply(A, B) == [[7, 0, 0], [-7, 0, 3]]
+
