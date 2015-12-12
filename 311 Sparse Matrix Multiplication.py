8f5af770e224ccdb00a367e3a16d48ed0a327a08
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-12-12 14:46:02 -0800

    take out description

--- 311 Sparse Matrix Multiplication.py
+++ 311 Sparse Matrix Multiplication.py
@@ -1,25 +1,6 @@
 """
-Given two sparse matrices A and B, return the result of AB.
-
-You may assume that A's column number is equal to B's row number.
-
-Example:
-
-A = [
-  [ 1, 0, 0],
-  [-1, 0, 3]
-]
-
-B = [
-  [ 7, 0, 0 ],
-  [ 0, 0, 0 ],
-  [ 0, 0, 1 ]
-]
-
-
-     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
-AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
-                  | 0 0 1 |
+Premium Question
+https://leetcode.com/problems/sparse-matrix-multiplication/
 """
 __author__ = 'Daniel'
 
