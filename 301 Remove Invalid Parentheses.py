cc89e8699eb595b058fb0d235227ed2ff6d979ce
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-12-17 12:19:03 -0800

    dfs

--- 301 Remove Invalid Parentheses.py
+++ 301 Remove Invalid Parentheses.py
@@ -15,12 +15,21 @@ __author__ = 'Daniel'
 class Solution(object):
     def removeInvalidParentheses(self, s):
         """
+        Algorithm focuses on left parentheses
+        Backtracking/DFS with pruning
         :type s: str
         :rtype: List[str]
         """
-        n = len(s)
-        # solve min count of removal
-        cnt = 0
+        rmcnt = self.minrm(s)
+        ret = []
+        self.dfs(s, "", 0, None, 0, rmcnt, ret)
+        return ret
+
+    def minrm(self, s):
+        """
+        returns minimal number of removals
+        """
+        rmcnt = 0
         l = 0
         for c in s:
             if c == "(":
@@ -29,38 +38,39 @@ class Solution(object):
                 l -= 1
                 if l < 0:
                     l = 0
-                    cnt += 1
+                    rmcnt += 1
 
-        cnt += l
-        ret = []
-        self.dfs(s, "", 0, None, 0, cnt, ret)
-        return ret
+        rmcnt += l
+        return rmcnt
 
-    def dfs(self, s, cur, l, removed, i, cnt, ret):
-        """backtracking, post-check"""
-        if l < 0 or cnt < 0 or i > len(s):
+    def dfs(self, s, cur, l, last_rm, i, rmcnt, ret):
+        """
+        Remove parenthesis
+        backtracking, post-check
+        :param s: original string
+        :param cur: current string builder
+        :param l: number of remaining left parentheses in s[0..i]
+        :param last_rm: last removed char
+        :param rmcnt: number of remaining removals needed
+        :param ret: results
+        """
+        if l < 0 or rmcnt < 0 or i > len(s):
             return
         if i == len(s):
-            if cnt == 0 and l == 0:
+            if rmcnt == 0 and l == 0:
                 ret.append(cur)
             return
 
-        if s[i] in ("(", ")"):
-            # jump
-            C = cnt
-            while i < len(s) and removed and removed == s[i]:
-                i += 1
-                C -= 1
-
-            if C != cnt:
-                self.dfs(s, cur, l, removed, i, C, ret)
-            else:
-                self.dfs(s, cur, l, s[i], i+1, cnt-1, ret)
-
-                L = l+1 if s[i] == "(" else l-1
-                self.dfs(s, cur+s[i], L, None, i+1, cnt, ret)  # put
+        if s[i] not in ("(", ")"):  # skip non-parenthesis
+            self.dfs(s, cur+s[i], l, None, i+1, rmcnt, ret)
         else:
-            self.dfs(s, cur+s[i], l, None, i+1, cnt, ret)
+            if last_rm == s[i]:  # jump, if rm, rm them all to avoid duplication
+                while i < len(s) and last_rm and last_rm == s[i]: i, rmcnt = i+1, rmcnt-1
+                self.dfs(s, cur, l, last_rm, i, rmcnt, ret)
+            else:
+                self.dfs(s, cur, l, s[i], i+1, rmcnt-1, ret)
+                L = l+1 if s[i] == "(" else l-1  # consume "("
+                self.dfs(s, cur+s[i], L, None, i+1, rmcnt, ret)  # put
 
 
 if __name__ == "__main__":
