a5c9637e78d6375396553f09a6879c4d7e5767b0
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-12-17 13:38:15 -0800

    topolocial sorting

--- 269 Alien Dictionary.py
+++ 269 Alien Dictionary.py
@@ -1,6 +1,8 @@
 """
 Premium Question
 """
+from collections import defaultdict
+
 __author__ = 'Daniel'
 
 
@@ -10,20 +12,52 @@ class Solution(object):
         :type words: List[str]
         :rtype: str
         """
-        V = {}
-        self.construct_graph(words, 0, len(words), 0, V)
+        V = self.construct_graph(words)
         visited = set()
-        marked = set()
+        pathset = set()
         ret = []
-
-        for k in V.keys():
-            if k not in visited:
-                if not self.topological_dfs(V, k, visited, marked, ret):
+        for v in V.keys():
+            if v not in visited:
+                if not self.topo_dfs(V, v, visited, pathset, ret):
                     return ""
 
         return "".join(reversed(ret))
 
-    def construct_graph(self, words, up, down, ptr, V):
+    def construct_graph(self, words):
+        V = defaultdict(list)
+        for i in xrange(len(words)-1):
+            for j in xrange(min(len(words[i]), len(words[i+1]))):
+                if words[i][j] != words[i+1][j]:
+                    V[words[i][j]].append(words[i+1][j])
+                    break  # need to break
+
+        return V
+
+    def topo_dfs(self, V, v, visited, pathset, ret):
+        """
+        Topological sort
+        :param V: Vertices HashMap
+        :param v: currently visiting letter
+        :param visited: visited letters
+        :param pathset: marked predecessor in the path
+        :param ret: the path, ordered  topologically
+        :return: whether contains cycles 
+        """
+        if v in pathset:
+            return False
+
+        pathset.add(v)
+        for nbr in V[v]:
+            if nbr not in visited:
+                if not self.topo_dfs(V, nbr, visited, pathset, ret):
+                    return False
+
+        pathset.remove(v)
+        visited.add(v)
+        ret.append(v)
+        return True
+
+    def construct_graph_tedious(self, words, up, down, ptr, V):
         """
         :param words:
         :param up: upper bound
@@ -44,34 +78,11 @@ class Solution(object):
                 while j < down and ptr < len(words[j]) and words[j][ptr] == words[i][ptr]:
                     j += 1
 
-                self.construct_graph(words, i, j, ptr+1, V)
+                self.construct_graph_tedious(words, i, j, ptr+1, V)
                 if j < down and ptr < len(words[j]):
                     V[words[i][ptr]].append(words[j][ptr])
 
                 i = j
-
-    def topological_dfs(self, V, cur, visited, pathset, ret):
-        """
-        :param V: Vertices HashMap
-        :param cur: currently visiting letter
-        :param visited: visited letters
-        :param pathset: marked predecessor in the path
-        :param ret: the path, ordered  topologically
-        :return: whether contains cycles 
-        """
-        if cur in pathset:
-            return False
-
-        pathset.add(cur)
-        for nei in V[cur]:
-            if nei not in visited:
-                if not self.topological_dfs(V, nei, visited, pathset, ret):
-                    return False
-
-        pathset.remove(cur)
-        visited.add(cur)
-        ret.append(cur)
-        return True
 
 
 if __name__ == "__main__":
