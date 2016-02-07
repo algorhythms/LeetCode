268662b82da5db30c292dfdb7320798beac3504a
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-02-06 23:53:18 -0800

    doc

--- 324 Wiggle Sort II.py
+++ 324 Wiggle Sort II.py
@@ -1,5 +1,5 @@
 """
-iven an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
 
 Example:
 (1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6].
