a7a30b84960e0b69a725bb7e57c8751e0a3d4df7
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-01-02 17:19:22 -0800

    437

---
+++ 437 Path Sum III.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go
+downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000
+to 1,000,000.
+"""
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        Brute force: two dfs, O(n^2)
+
+        Prefix sum in Tree, starting from root - O(n)
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        count = [0]  # pass as a reference
+        self.dfs(root, sum, 0, {}, count)
+        return count[0]
+
+    def dfs(self, root, sum, cur_sum, prefix_sum, count):
+        """
+        Root to node sum
+        prefix_sum: Dict[int, int], sum -> count
+        """
+        if not root:
+            return
+
+        cur_sum += root.val
+        prefix_sum[cur_sum] = prefix_sum.get(cur_sum, 0) + 1
+        # ∃ prefix_sum: cur_sum - prefix_sum = sum
+        diff = cur_sum - sum
+        if diff in prefix_sum:
+            count[0] += prefix_sum[diff]
+        if diff == 0:  # trivial case
+            count[0] += 1
+
+        self.dfs(root.left, sum, cur_sum, prefix_sum, count)
+        self.dfs(root.right, sum, cur_sum, prefix_sum, count)
+        prefix_sum[cur_sum] -= 1  # pop to save space
