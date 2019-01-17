d2b9793c755c7a0b36eb980b78226762fae7e33e
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-01-16 19:58:39 -0800

    413

---
+++ 413 Arithmetic Slices.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A sequence of number is called arithmetic if it consists of at least three
+elements and if the difference between any two consecutive elements is the same.
+"""
+
+class Solution:
+    def count(self, l):
+        return (l-1) * l // 2
+
+    def numberOfArithmeticSlices(self, A):
+        """
+        Diff the array, find the pattern.
+        Find that it is a function of length of the sequence
+        With 3 consecutive sequence (l - 1) * l / 2
+        
+        :type A: List[int]
+        :rtype: int
+        """
+        ret = 0
+        if len(A) < 3:
+            return ret
+
+        delta = []
+        for i in range(1, len(A)):
+            delta.append(A[i] - A[i-1])
+
+        s = 0
+        e = 0
+        while s < len(delta):
+            while e < len(delta) and delta[s] == delta[e]:
+                e += 1
+
+            l = e - s
+            ret += self.count(l)
+
+            s = e
+
+        return ret
+
+
+if __name__ == "__main__":
+    assert Solution().numberOfArithmeticSlices([1, 2, 3, 4]) == 3
