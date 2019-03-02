bd047570a5da1dacf0fb3bf556d5e51a88662f51
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-03-02 13:49:17 -0800

    703

--- 704 Binary Search.py
+++ 704 Binary Search.py
@@ -29,3 +29,15 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return -1
