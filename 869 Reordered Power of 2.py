a7620dcf7bc6b2a06b17e8cb5613c65a7c9cb5d9
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-03-22 23:33:44 -0700

    869

---
+++ 869 Reordered Power of 2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Starting with a positive integer N, we reorder the digits in any order (including the original order) such that the leading digit is not zero.
+
+Return true if and only if we can do this in a way such that the resulting number is a power of 2.
+
+
+
+Example 1:
+
+Input: 1
+Output: true
+Example 2:
+
+Input: 10
+Output: false
+Example 3:
+
+Input: 16
+Output: true
+Example 4:
+
+Input: 24
+Output: false
+Example 5:
+
+Input: 46
+Output: true
+
+
+Note:
+
+1 <= N <= 10^9
+"""
+from collections import Counter
+
+
+class Solution:
+    def reorderedPowerOf2(self, N: int) -> bool:
+        """
+        count the digit and compare
+        """
+        counts = Counter(str(N))
+        for i in range(31):  # 32 bit unsighed int 
+            if counts == Counter(str(1 << i)):
+                return True
+        else:
+            return False
