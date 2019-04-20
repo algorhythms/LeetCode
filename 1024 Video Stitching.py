e942a4941c14fc405c6fba8173b52188c7da1dcf
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-04-20 00:30:47 -0700

    1024

---
+++ 1024 Video Stitching.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+"""
+You are given a series of video clips from a sporting event that lasted T
+seconds.  These video clips can be overlapping with each other and have varied
+lengths.
+
+Each video clip clips[i] is an interval: it starts at time clips[i][0] and ends
+at time clips[i][1].  We can cut these clips into segments freely: for example,
+a clip [0, 7] can be cut into segments [0, 1] + [1, 3] + [3, 7].
+
+Return the minimum number of clips needed so that we can cut the clips into
+segments that cover the entire sporting event ([0, T]).  If the task is
+impossible, return -1.
+
+Example 1:
+Input: clips = [[0,2],[4,6],[8,10],[1,9],[1,5],[5,9]], T = 10
+Output: 3
+Explanation:
+We take the clips [0,2], [8,10], [1,9]; a total of 3 clips.
+Then, we can reconstruct the sporting event as follows:
+We cut [1,9] into segments [1,2] + [2,8] + [8,9].
+Now we have segments [0,2] + [2,8] + [8,10] which cover the sporting event [0, 10].
+Example 2:
+
+Input: clips = [[0,1],[1,2]], T = 5
+Output: -1
+Explanation:
+We can't cover [0,5] with only [0,1] and [0,2].
+Example 3:
+
+Input: clips = [[0,1],[6,8],[0,2],[5,6],[0,4],[0,3],[6,7],[1,3],[4,7],[1,4],[2,5],[2,6],[3,4],[4,5],[5,7],[6,9]], T = 9
+Output: 3
+Explanation:
+We can take clips [0,4], [4,7], and [6,9].
+Example 4:
+
+Input: clips = [[0,4],[2,8]], T = 5
+Output: 2
+Explanation:
+Notice you can have extra video after the event ends.
+
+Note:
+1 <= clips.length <= 100
+0 <= clips[i][0], clips[i][1] <= 100
+0 <= T <= 100
+"""
+from typing import List
+
+
+class Solution:
+    def videoStitching(self, clips: List[List[int]], T: int) -> int:
+        """
+        Greedy is correct. The larger the coverage, the better
+        """
+        clips.sort()
+        prev_e = 0
+        ret = 0
+
+        i = 0
+        while i < len(clips):
+            if clips[i][0] > prev_e:  # gap
+                break
+
+            max_e = -float("inf")
+            while i < len(clips) and clips[i][0] <= prev_e:
+                max_e = max(max_e, clips[i][1])
+                i += 1
+
+            prev_e = max_e  # take
+            ret += 1
+            if prev_e >= T:
+                break
+
+        return ret if prev_e >= T else -1
+
+    def videoStitching_error(self, clips: List[List[int]], T: int) -> int:
+        """
+        gready take the max coverage?
+        """
+        A = [(s, -e, s, e) for s, e in clips]
+        A.sort()
+        ret = 1
+        _, _, prev_s, prev_e = A[0]
+        if prev_s > 0:
+            return False
+
+        for _, _, s, e in A[1:]:
+            if s <= prev_e and e > prev_e:
+                prev_e = e
+                ret += 1
+
+
+if __name__ == "__main__":
+    assert Solution().videoStitching([[0,4],[2,8]], 5) == 2
